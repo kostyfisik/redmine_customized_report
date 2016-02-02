54998d1b9d005676f1f3c3d3b19e1c555920acdc
Konstantin Ladutenko <kostyfisik@users.noreply.github.com>
2016-02-02 17:11:51 +0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # Plugin's routes
 # See: http://guides.rubyonrails.org/routing.html
 RedmineApp::Application.routes.draw do
-match 'projects/:id/issues/customized_reports',:to => 'customized_reports#index', :as => 'customized_reports', via: :get
+match 'projects/:id/issues/customized_reports',:to => 'customized_reports#index', :as => 'customized_reports', via: [:get, :post]
 resources :projects do
 resources :report_queries
 end
